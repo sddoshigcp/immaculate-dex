7a68e6a08b6e4dfffcb92c0927859f97f403753d
Siddhant Doshi <sddoshi2@ncsu.edu>
2023-08-10 19:15:21 -0400

    Styled dropdown, added sprites

--- immaculate-dex-v2/client/src/Search/SearchResult.jsx
+++ immaculate-dex-v2/client/src/Search/SearchResult.jsx
@@ -1,4 +1,6 @@
 import "./SearchResult.css";
+import { getSprite } from "../Grid/Pokemon";
+import { useEffect, useState } from "react";
 
 // export const SearchResult = ({ result }) => {
 //   return (
@@ -12,9 +14,21 @@ import "./SearchResult.css";
 // };
 
 export const SearchResult = ({ result, onSelect }) => {
+  const [pokemonSprite, setPokemonSprite] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const sprite = await getSprite(result);
+      setPokemonSprite(sprite);
+    };
+    fetchData();
+  }, []);
+
   return (
       <div className="search-result" onClick={onSelect}>
-          {result}
+          <p className="pokemon-name">{result.toUpperCase()}</p>
+          <img className="sprite-image" src={pokemonSprite} alt="pokemon" />
+          <button className="select-pokemon" onClick={onSelect}>Select</button>
       </div>
   );
 };
