87729f07d5a8ee18f6a919fbe9295e1737d9149d
Siddhant Doshi <sddoshi2@ncsu.edu>
2023-08-11 00:24:38 -0400

    fixed pokemon sprite loading issue

--- immaculate-dex-v2/client/src/Search/SearchResult.jsx
+++ immaculate-dex-v2/client/src/Search/SearchResult.jsx
@@ -2,32 +2,35 @@ import "./SearchResult.css";
 import { getSprite } from "../Grid/Pokemon";
 import { useEffect, useState } from "react";
 
-// export const SearchResult = ({ result }) => {
-//   return (
-//     <div
-//       className="search-result"
-//       onClick={(e) => alert(`You selected ${result}!`)}
-//     >
-//       {result}
-//     </div>
-//   );
-// };
 
 export const SearchResult = ({ result, onSelect }) => {
   const [pokemonSprite, setPokemonSprite] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const sprite = await getSprite(result);
+  //     setPokemonSprite(sprite);
+  //     setLoading(false);
+  //   };
+  //   fetchData();
+  // }, []);
 
   useEffect(() => {
     async function fetchData() {
+      setLoading(true);
       const sprite = await getSprite(result);
       setPokemonSprite(sprite);
-    };
+      setLoading(false);
+    }
     fetchData();
-  }, []);
+  }, [result]);
+  
 
   return (
-      <div className="search-result" onClick={onSelect}>
+      <div className="search-result">
           <p className="pokemon-name">{result.toUpperCase()}</p>
-          <img className="sprite-image" src={pokemonSprite} alt="pokemon" />
+          {!loading && <img className="sprite-image" key={pokemonSprite} src={pokemonSprite} alt="pokemon" />}
           <button className="select-pokemon" onClick={onSelect}>Select</button>
       </div>
   );
